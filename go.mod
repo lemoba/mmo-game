a2ff85ebec4626e1146d01a1762fc58327897929
ranen <189527031@qq.com>
2023-03-02 17:34:21 +0800

    feat: add msg proto

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/lemoba/mmo-game
 go 1.19
 
 require google.golang.org/protobuf v1.28.1
+
+require github.com/lemoba/zinx v1.0.0 // indirect
--- proto/main.go
+++ proto/main.go
@@ -1,46 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"github.com/lemoba/mmo-game/proto/pb"
-	"google.golang.org/protobuf/proto"
-)
+import "github.com/lemoba/zinx/znet"
 
 func main() {
-	// 定义一个Person结构体
-	person := &pb.Person{
-		Name:   "ranen",
-		Age:    27,
-		Emails: []string{"lemoba@qq.com", "ranen1024@gmail.com"},
-		Phones: []*pb.PhoneNumber{
-			&pb.PhoneNumber{
-				Number: "15549959926",
-				Type:   pb.PhoneType_MOBILE,
-			},
-			&pb.PhoneNumber{
-				Number: "15549959927",
-				Type:   pb.PhoneType_WORK,
-			},
-			&pb.PhoneNumber{
-				Number: "15549959928",
-				Type:   pb.PhoneType_HOME,
-			},
-		},
-	}
+	// 创建zinx
+	s := znet.NewServer("MMO Game")
 
-	// 编码
-	// 将对象进行序列化，得到一个二进制文件，进行传输
-	data, err := proto.Marshal(person)
-	if err != nil {
-		fmt.Println("Marshal error: ", err)
-	}
-
-	// 解码
-	newData := &pb.Person{}
-	if err := proto.Unmarshal(data, newData); err != nil {
-		fmt.Println("unmarshal error: ", err)
-	}
-
-	fmt.Println("源数据: ", person)
-	fmt.Println("newData: ", newData)
+	// 创建HOOK函数
+	// 注册路由业务
+	// 启动服务
+	s.Start()
 }
